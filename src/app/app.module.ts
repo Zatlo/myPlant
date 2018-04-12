6936ecff8982cd04346d30fa44da83fa846c33db
Kevin <valdezkevin102@gmail.com>
2018-04-12 13:43:09 -0700

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { WeatherProvider } from '../providers/weather/weather';
 
+//import firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+//firebase configuaration
+var config = {
+  apiKey: "AIzaSyBip5RposPD4fwJWYwvaXoAtCcb55hzM7k",
+  authDomain: "myplant-1dcc9.firebaseapp.com",
+  databaseURL: "https://myplant-1dcc9.firebaseio.com",
+  projectId: "myplant-1dcc9",
+  storageBucket: "myplant-1dcc9.appspot.com",
+  messagingSenderId: "115928435512"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +39,9 @@ import { WeatherProvider } from '../providers/weather/weather';
   imports: [
     BrowserModule,
     HttpClientModule, // import httpmodule
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config), //import angular modules
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
